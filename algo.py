d68da957b39661f07d30932e08fe38ea8deafa76
ydidwania <didwaniayashvardhan+github@gmail.com>
2019-10-10 22:44:21 +0530

    ucb-bv1 with cost incl

--- algo.py
+++ algo.py
@@ -1,10 +1,12 @@
 import numpy.random as nprand
 import numpy as np
 import argparse
-import math
+from math import sqrt
 
 pho_s = 100.0
 pho_l = 200.0
+cost_s = 80.0
+cost_l = 160.0
 
 
 class Bandit(object) :
@@ -86,7 +88,7 @@ def ucb(bandit, horizon, *args):
         if i<nb :
             arm = i
         else :
-            ucb_a = [abs(p_emp[arm] - 0.5) + math.sqrt(2*ln(i)/n_pulls[arm]) for arm in range(0,nb)]
+            ucb_a = [abs(p_emp[arm] - 0.5) + sqrt(2*ln(i)/n_pulls[arm]) for arm in range(0,nb)]
             arm = ucb_a.index(max(ucb_a))
         
         win = bandit.pull(arm)
@@ -142,15 +144,54 @@ def thompson_sampling(bandit, horizon, seed, *args):
     
     return rew
 
+def ucb_bv1(bandit, horizon, *args):
+    nb = bandit.n_bandits
+    reg = 0
+    wins,earnings = 0,0
+    avg_l_price = 0.0
+    n_pulls = [0]*nb
+    ucb_a = [0.0]*nb
+    cost, rew = [0.0]*nb, [0.0]*nb 
+    lmbda = 1/(nb-1)
+    budget = 1;
+    D = lambda i,r,c,n : (r/c) + ((1+1/lmbda)*sqrt(ln(i)/n))/(lmbda - sqrt(ln(i)/n)) 
+    for i in range(0, horizon):
+
+        if i<nb :
+            arm = i
+        elif sum(cost) >= 100:
+            arm = 0
+            budget = 0;
+        else :
+            ucb_a = [D(i,rew[arm], max(cost[arm],1e-6), n_pulls[arm]) for arm in range(nb)]
+            arm = ucb_a.index(max(ucb_a))
+        
+        win = bandit.pull(arm)
+        price = bandit.get_price(arm)
+        wins += win
+        cost[arm] += win*0 + (1 - win)*1.0
+        rew[arm] += (win*(price-cost_l) + (1 - win)*(pho_s-cost_s) -  pho_s + cost_s)/(pho_l -cost_l- pho_s + cost_s)
+        n_pulls[arm] += 1
+        reg += (win*(pho_l - price)/(pho_l - pho_s)) + (1-win)*1
+
+        if (win):
+            avg_l_price = (avg_l_price*(wins-1) + (price))/wins
+        
+        print (" N = %d, Offered_price = %.2f, result=%d, Small=%d, Regret=%.2f, Budget=%d"%(i, price, win, 100-(i+1-wins), reg, budget))
+
+    earnings = sum(rew)*(pho_l -cost_l- pho_s + cost_s) + 200*(pho_s - cost_s)
+    print ("Total Earnings = ", earnings)
+    print ("Avg Large Price = ", avg_l_price) 
+    return reg
 
 
-algorithms = [round_robin, epsilon_greedy, ucb, kl_ucb, thompson_sampling]
+
+algorithms = [round_robin, epsilon_greedy, ucb, ucb_bv1, kl_ucb, thompson_sampling]
 algorithms = {algo.__name__.replace('_','-'): algo for algo in algorithms}
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--instance',required=True)
 parser.add_argument('--algorithm', choices=list(algorithms.keys()), required=True)
-parser.add_argument('--randomSeed', type=int, required=True)
 parser.add_argument('--epsilon', type=float, required=True)
 parser.add_argument('--horizon', type=int, required=True)
 args = parser.parse_args()
@@ -158,8 +199,8 @@ bandit_file = open(args.instance, 'r')
 lines = bandit_file.readlines()
 algorithm = args.algorithm.replace('-','_')
 
-bandit_instance = Bandit(len(lines), [float(p) for p in lines], args.randomSeed, [pho_s + (pho_l-pho_s)*i/19.0 for i in range(len(lines))])
-algo_randSeed = args.randomSeed * 2
-rew = algorithms[args.algorithm](bandit_instance, args.horizon, algo_randSeed, args.epsilon)
+bandit_instance = Bandit(len(lines), [float(p) for p in lines], 20, [pho_s + (pho_l-pho_s)*i/19.0 for i in range(len(lines))])
+
+rew = algorithms[args.algorithm](bandit_instance, args.horizon, 40, args.epsilon)
 # reg = bandit_instance.get_max_reward(args.horizon) - rew
 # print ("%s, %s, %d, %s, %d, %s\n" %(args.instance, args.algorithm, args.randomSeed, args.epsilon, args.horizon, reg ))
